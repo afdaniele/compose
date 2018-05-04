e3abb491d47d2677b20e7a27d328c31ec3b637c9
Andrea F. Daniele <afdaniele@ttic.edu>
2018-05-04 17:41:39 -0500

    cleaned up Configuration class

--- public_html/system/classes/Configuration.php
+++ public_html/system/classes/Configuration.php
@@ -8,15 +8,10 @@
 
 namespace system\classes;
 
-require_once __DIR__.'/jsonDB/JsonDB.php';
-use system\classes\jsonDB\JsonDB;
-
 require_once __DIR__.'/libs/booleanval.php';
 
 
 class Configuration {
-
-	private static $jsondb = null;
 
 	private static $initialized = false;
 
@@ -42,11 +37,6 @@ class Configuration {
 	public static $NOREPLY_MAIL_PASSWORD;
 	public static $NOREPLY_MAIL_SECURE_PROTOCOL;
 	public static $NOREPLY_MAIL_SERVER_PORT;
-
-	public static $MAINTEINANCE_MODE;
-	public static $MAIN_PAGE_TITLE;
-	public static $ADMIN_CONTACT_MAIL_ADDRESS;
-	public static $CACHE_ENABLED;
 
 	public static $IS_MOBILE = false;
 
@@ -100,15 +90,6 @@ class Configuration {
 			self::$CACHE_SYSTEM = $CACHE_SYSTEM;
 			self::$WEBAPI_VERSION = $WEBAPI_VERSION;
 			//
-			// load the custom setting
-			self::$jsondb = new JsonDB( __DIR__.'/../config/configuration.json' );
-
-			// Dashboard configuration
-			self::$MAINTEINANCE_MODE = booleanval( self::$jsondb->get('maintenance_mode', $DEFAULT_VALUES_DASHBOARD_CONFIG['maintenance_mode']) );
-			self::$MAIN_PAGE_TITLE = self::$jsondb->get('main_page_title', $DEFAULT_VALUES_DASHBOARD_CONFIG['main_page_title']);
-			self::$ADMIN_CONTACT_MAIL_ADDRESS = self::$jsondb->get('admin_contact_email_address', $DEFAULT_VALUES_DASHBOARD_CONFIG['admin_contact_email_address']);
-			self::$CACHE_ENABLED = booleanval( self::$jsondb->get('cache_enabled', $DEFAULT_VALUES_DASHBOARD_CONFIG['cache_enabled']) );
-			//
 			self::$initialized = true;
 			//
 			return array( 'success' => true );
@@ -116,34 +97,6 @@ class Configuration {
 			return array( 'success' => true );
 		}
 	}//init
-
-	public static function get( $key, $default=null ){
-		if( self::$jsondb != null ){
-			if( self::$jsondb->contains($key) ){
-				$val = self::$jsondb->get($key, $default);
-				return array('success' => true, 'data' => $val);
-			}else{
-				return array('success' => false, 'data' => 'Parameter unknown');
-			}
-		}
-		return array('success' => false, 'data' => 'An error occurred while reading the configurations. Please, retry!');
-	}//get
-
-	public static function set( $key, $val ){
-		if( self::$jsondb != null ){
-			self::$jsondb->set($key, $val);
-			return array('success' => true );
-		}
-		return array('success' => false, 'data' => 'An error occurred while writing the configurations. Please, retry!');
-	}//set
-
-	public static function commit(){
-		if( self::$jsondb != null ){
-			$res = self::$jsondb->commit();
-			return $res;
-		}
-		return array('success' => false, 'data' => 'An error occurred while writing the configurations. Please, retry!');
-	}//commit
 
 }//Configuration
 
