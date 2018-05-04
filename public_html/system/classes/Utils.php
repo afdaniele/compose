323e04f792f07274773914cc731a16d5c569c616
Andrea F. Daniele <afdaniele@ttic.edu>
2018-05-04 17:41:13 -0500

    added string_to_valid_filename as utility function

--- public_html/system/classes/Utils.php
+++ public_html/system/classes/Utils.php
@@ -26,6 +26,19 @@ class Utils{
         return $matches[$groupNum][0];
     }//regex_extract_group
 
+	public static function string_to_valid_filename($string){
+		//remove non alpha numeric characters
+		$string = preg_replace("/[^A-Za-z0-9 _]/", '', $string);
+		//lowercase
+		$string = strtolower($string);
+		//replace more than one space to underscore
+		$string = preg_replace('/([\s])\1+/', '_', $string );
+		//convert any single spaces to underscrore
+		$string = str_replace(" ","_",$string);
+		// return sanitized string
+		return $string;
+	}//string_to_valid_filename
+
 }//Utils
 
 ?>
