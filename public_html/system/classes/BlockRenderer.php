fc0baaa133b115c107887dd362b3c41dbce14568
Andrea F. Daniele <afdaniele@ttic.edu>
2018-05-04 17:37:26 -0500

    added block renderers

---
+++ public_html/system/classes/BlockRenderer.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace system\classes;
+
+class BlockRenderer{
+
+    static protected $ICON = [
+        "class" => "fa",
+        "name" => "window-maximize"
+    ];
+
+    static protected $ARGUMENTS = [];
+
+    private $mission_control;
+
+    function __construct( $mission_control ){
+        $this->mission_control = $mission_control;
+    }//__construct
+
+    public function draw( $class, $id, $title, $subtitle, &$shape, &$args=[], $show_header=True ){
+        $args['title'] = $title;
+        $args['subtitle'] = $subtitle;
+        ?>
+        <div class="block_renderer_canvas text-center <?php echo $class ?>" id="<?php echo $id ?>" data-renderer="<?php echo get_called_class() ?>">
+            <table>
+                <tr class="block_renderer_header text-left">
+                    <td>
+                        <?php
+                        if( $show_header ){
+                            ?>
+                            <i class="<?php echo self::get_icon_class() ?>" aria-hidden="true"></i>
+                            <?php
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <h5><?php echo $title ?></h5>
+                        <h6><?php echo $subtitle ?></h6>
+                    </td>
+                    <td>
+                        <div class="btn-group">
+                            <a class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                            </a>
+                            <ul class="dropdown-menu" style="color:black">
+
+                                <h4 style="padding-left:12px; font-weight:normal">Size:</h4>
+
+                                <div class="btn-group" data-toggle="buttons" style="width:100%">
+
+                                    <?php
+                                    $options = $this->mission_control->get_available_shapes();
+
+                                    $num_options = count($options);
+                                    $num_cols = 3;
+                                    $num_rows = intval(ceil($num_options/$num_cols));
+                                    $figure_max_size_px = 22;
+                                    $option_max_rc = 1;
+                                    foreach ($options as $opt) {
+                                        if( $opt[0] > $option_max_rc )
+                                            $option_max_rc = $opt[0];
+                                        if( $opt[1] > $option_max_rc )
+                                            $option_max_rc = $opt[1];
+                                    }
+                                    ?>
+                                    <table>
+                                        <?php
+                                        for ($i = 0; $i < $num_rows; $i++) {
+                                            echo "<tr>";
+                                            for ($j = 0; $j < $num_cols; $j++) {
+                                                $k = $j + $i*$num_cols;
+                                                if( $k >= $num_options ){
+                                                    echo "<td></td>";
+                                                    continue;
+                                                }
+                                                // create option button
+                                                $option = $options[$k];
+                                                $w = ($option[1] / $option_max_rc) * $figure_max_size_px;
+                                                $h = ($option[0] / $option_max_rc) * $figure_max_size_px;
+                                                $is_active = boolval( $shape['rows']==$option[0] && $shape['cols']==$option[1] );
+                                                ?>
+                                                <td class="text-center" style="width:<?php echo 100/$num_cols ?>%">
+                                                    <label class="btn btn-default <?php echo $is_active? 'active' : '' ?>"
+                                                        style="width:<?php echo $figure_max_size_px+20 ?>px; padding:5px; margin:4px auto"
+                                                        onclick="mission_control_switch_shape('<?php echo $id ?>', '<?php echo $option[0] ?>', '<?php echo $option[1] ?>' );">
+
+                                                        <div style="width:100%; height:<?php echo $figure_max_size_px ?>px; vertical-align:middle; display:grid">
+                                                            <div style="width:<?php echo $w ?>px; height:<?php echo $h ?>px; background-color:black; margin:auto"></div>
+                                                        </div>
+                                                        <input type="radio" name="options" id="option<?php echo $k ?>" autocomplete="off" style="display:none" <?php echo $is_active? 'checked' : '' ?>>
+                                                        <?php echo sprintf("%dx%d", $option[0], $option[1]) ?>
+
+                                                    </label>
+                                                </td>
+                                                <?php
+                                            }
+                                            echo "</tr>";
+                                        }
+                                        ?>
+                                    </table>
+                                </div>
+
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a role="button" data-toggle="modal" data-target="#<?php echo $id ?>_properties_modal">
+                                        <span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>
+                                        &nbsp;
+                                        Properties
+                                    </a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                                        &nbsp;
+                                        Dispose
+                                    </a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+
+                    </td>
+                </tr>
+                <tr class="block_renderer_container">
+                    <td colspan="3">
+                        <?php static::render( $id, $args ); ?>
+                    </td>
+                </tr>
+            </table>
+
+
+            <div class="modal fade mission-control-block-properties-modal" id="<?php echo $id ?>_properties_modal" tabindex="-1" role="dialog" aria-hidden="true">
+        		<div class="modal-dialog">
+        			<div class="modal-content">
+        				<div class="modal-header">
+        					<button type="button" class="close" data-dismiss="modal">
+                                <span aria-hidden="true">&times;</span>
+                                <span class="sr-only">Close</span>
+                            </button>
+        					<h4 class="modal-title">
+                                Properties - "<?php echo $title ?>"
+                            </h4>
+        				</div>
+
+        				<div class="modal-body text-left">
+        					<?php
+                            // create layout for form
+                            $layout = [
+                                'title' => [
+                                    'type' => 'text',
+                                    'editable' => true,
+                                    'value' => $title,
+                                    'name' => 'Block title'
+                                ],
+                                'subtitle' => [
+                                    'type' => 'text',
+                                    'editable' => true,
+                                    'value' => $subtitle,
+                                    'name' => 'Block subtitle'
+                                ]
+                            ];
+                            // append custom arguments to the layout
+                            foreach (static::$ARGUMENTS as $key => $desc) {
+                                $layout_key = $key;
+                                $layout_desc = [
+                                    'type' => $desc['type'],
+                                    'editable' => True,
+                                    'value' => $args[$key],
+                                    'name' => $desc['name']
+                                ];
+                                // add item to the layout
+                                $layout[$layout_key] = $layout_desc;
+                            }
+                            $formID = null;
+                            $formClass = 'mission-control-block-properties-form';
+                            $method = 'post';
+                            // generate form
+        					generateFormByLayout( $layout, $formID, $formClass, $method, $action=null, $values=$args );
+        					?>
+        				</div>
+
+        				<div class="modal-footer">
+        					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        					<button type="button" class="btn btn-success" id="save-button">Save</button>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+
+        <script type="text/javascript">
+            document.getElementById("<?php echo $id ?>").addEventListener(
+                'DOMAttrModified',
+                function(e){
+                    console.log('prevValue: ' + e.prevValue, 'newValue: ' + e.newValue);
+                },
+                false
+            );
+        </script>
+        <?php
+    }//draw
+
+    protected static function render( $id, &$args ){
+        ?>
+        <p style="position:relative; top:50%; transform:translateY(-50%)">
+            Your renderer must implement the method <code>render()</code>.
+        </p>
+        <?php
+    }//render
+
+    final public function get_icon_class(){
+        return sprintf('%s %s-%s', static::$ICON['class'], static::$ICON['class'], static::$ICON['name']);
+    }//get_icon_class
+
+}//BlockRenderer
+?>
---
+++ public_html/system/classes/MissionControl.php
@@ -0,0 +1,258 @@
+<?php
+
+use \system\classes\Core as Core;
+use \system\classes\BlockRenderer as BlockRenderer;
+
+
+class MissionControl{
+
+    private $grid_id;
+    private $grid_width;
+    private $resolution;
+    private $block_gutter;
+    private $block_size;
+    private $block_border_thickness;
+    private $sizes_available;
+    private $blocks;
+
+    function __construct( $grid_id, $grid_width, $resolution, $block_gutter, $block_border_thickness, $sizes_available, $blocks=[] ){
+        $this->grid_id = $grid_id;
+        $this->grid_width = $grid_width;
+        $this->resolution = $resolution;
+        $this->block_gutter = $block_gutter;
+        $this->block_size = $block_size;
+        $this->block_border_thickness = $block_border_thickness;
+        $this->block_size = ( $grid_width - ($resolution-1)*$block_gutter ) / $resolution;
+        $this->sizes_available = $sizes_available;
+        $this->blocks = $blocks;
+    }//__construct
+
+    function create(){
+        $header_h = 40;
+        // load all block renderers registered
+        Core::loadPackagesModules( 'renderers/blocks' );
+        // get all block renderers registered
+        $renderers_available = Core::getClasses( 'system\classes\BlockRenderer' );
+        ?>
+        <div id="<?php echo $this->grid_id ?>" class="mission-control-grid">
+            <?php
+            $empty_renderer = new EmptyRenderer( $this );
+            foreach ($this->blocks as $block){
+                $rand_id = Core::generateRandomString(8);
+                $block_args = $block['args'];
+                if( !in_array($block['renderer'], $renderers_available) ){
+                    $renderer = $empty_renderer;
+                    $block_args = [
+                        'message' => sprintf('Renderer <code>%s</code> not found!', $block['renderer'])
+                    ];
+                }else{
+                    $renderer = new $block['renderer']( $this );
+                }
+                // draw block
+                $renderer->draw(
+                    sprintf(
+                        'mission-control-item mission-control-item-r%d-c%d',
+                        $block['shape']['rows'],
+                        $block['shape']['cols']
+                    ),
+                    $rand_id,
+                    $block['title'],
+                    $block['subtitle'],
+                    $block['shape'],
+                    $block_args
+                );
+                ?>
+            <?php
+            }
+            ?>
+        </div>
+
+        <script type="text/javascript">
+            $( document ).ready(function() {
+                // create grid
+                var grid = $('#<?php echo $this->grid_id ?>').packery({
+                  itemSelector: '.mission-control-item',
+                  columnWidth: <?php echo $this->block_size ?>,
+                  gutter: <?php echo $this->block_gutter ?>
+                });
+
+                // make all grid-items draggable
+                grid.find('.mission-control-item').each( function( i, gridItem ) {
+                  var draggie = new Draggabilly( gridItem );
+                  // bind drag events to Packery
+                  grid.packery( 'bindDraggabillyEvents', draggie );
+                });
+            });
+
+
+            function mission_control_switch_shape( box_id, new_rows, new_cols ){
+                // get box
+                box = $('#'+box_id);
+                // remove previous class
+                box.removeClass( function(_, classes){
+                    classes_to_remove = (classes.match(/\s*mission-control-item-r([0-9]+)-c([0-9]+)\s*/g) || []).join(' ');
+                    return classes_to_remove;
+                } );
+                // add new class
+                box.addClass( "mission-control-item-r{0}-c{1}".format( new_rows, new_cols ) );
+                // update the grid
+                box.closest('.mission-control-grid').packery();
+            }
+        </script>
+
+
+        <style type="text/css">
+
+            /* ---- grid ---- */
+
+            #<?php echo $this->grid_id ?> {
+              background: inherit;
+              max-width: 100%;
+            }
+
+            /* clear fix */
+            #<?php echo $this->grid_id ?>:after {
+              content: '';
+              display: block;
+              clear: both;
+            }
+
+            /* ---- .mission-control-item ---- */
+
+            .mission-control-item {
+              float: left;
+              background: #fff;
+              border: <?php echo $this->block_border_thickness ?>px solid hsla(0, 0%, 80%, 0.5);
+            }
+
+            <?php
+            for ($i = 1; $i < $this->resolution+1; $i++) {
+                for ($j = 1; $j < $this->resolution+1; $j++) {
+                    $h = $i*$this->block_size+($i-1)*$this->block_gutter;
+                    $w = $j*$this->block_size+($j-1)*$this->block_gutter;
+                    $header_w = $w - 2*40 - 2*$this->block_border_thickness;
+                    echo sprintf("
+                        .mission-control-item-r%d-c%d{
+                            min-height: %dpx;
+                            min-width: %dpx;
+                            height: %dpx;
+                            width: %dpx;
+                            max-height: %dpx;
+                            max-width: %dpx;
+                        }
+
+                        .mission-control-item-r%d-c%d .block_renderer_header > td h5,
+                        .mission-control-item-r%d-c%d .block_renderer_header > td h6{
+                            min-width: %dpx;
+                            width: %dpx;
+                            max-width: %dpx;
+                        }
+
+                        .mission-control-item-r%d-c%d .block_renderer_container .resizable{
+                            max-width: %dpx;
+                            max-height: %dpx;
+                        }
+                        ", $i, $j,
+                        $h,
+                        $w,
+                        $h,
+                        $w,
+                        $h,
+                        $w,
+                        $i, $j,
+                        $i, $j,
+                        $header_w,
+                        $header_w,
+                        $header_w,
+                        $i, $j,
+                        $w,
+                        $h-$header_h  //TODO: we may want to consider the case in which the header is hidden
+                    );
+                }
+            }
+            ?>
+
+            .mission-control-item:hover {
+              cursor: move;
+            }
+
+            .mission-control-item.is-dragging,
+            .mission-control-item.is-positioning-post-drag {
+              background: #fec612;
+              z-index: 2;
+            }
+
+            .packery-drop-placeholder{
+              outline: 3px dashed hsla(0, 0%, 0%, 0.5);
+              outline-offset: -6px;
+              -webkit-transition: -webkit-transform 0.2s;
+                      transition: transform 0.2s;
+            }
+
+            .block_renderer_canvas table{
+                width: 100%;
+                height: 100%;
+            }
+
+            .block_renderer_canvas .block_renderer_header > td:first-of-type,
+            .block_renderer_canvas .block_renderer_header > td:last-of-type{
+                width: <?php echo $header_h ?>px;
+                padding: 10px;
+                text-align: center;
+            }
+
+            .block_renderer_canvas .block_renderer_header > td:last-of-type{
+                padding: 0;
+            }
+
+            .block_renderer_canvas .block_renderer_header > td:first-of-type i{
+                font-size: 20px
+            }
+
+            .block_renderer_canvas .block_renderer_header > td:last-of-type a.dropdown-toggle{
+                font-size: 18px;
+                color: inherit;
+            }
+
+            .block_renderer_canvas .block_renderer_header > td:last-of-type .dropdown-menu li.divider{
+                margin: 5px 0;
+            }
+
+            .block_renderer_canvas .block_renderer_header > td h5,
+            .block_renderer_canvas .block_renderer_header > td h6{
+                margin: 0;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+            }
+
+            .block_renderer_canvas .block_renderer_container{
+                height: 100%;
+            }
+
+            .block_renderer_canvas .block_renderer_container td:first-of-type{
+                text-align: center;
+                vertical-align: middle;
+            }
+
+
+            .block_renderer_canvas .block_renderer_header .dropdown-toggle:active,
+            .block_renderer_canvas .block_renderer_header .dropdown-toggle.active{
+                background-image: none;
+                outline: 0;
+                -webkit-box-shadow: none;
+                box-shadow: none;
+            }
+        </style>
+        <?php
+    }//create
+
+    public function get_available_shapes(){
+        return $this->sizes_available;
+    }//get_available_shapes
+
+}//MissionControl
+
+
+
+?>
