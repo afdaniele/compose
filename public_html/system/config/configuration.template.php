737bbaf7cc3bb72d38e36c328a67c9cb2cafb236
Andrea F. Daniele <afdaniele@ttic.edu>
2018-07-18 16:39:30 -0500

    added template configuration file

---
+++ public_html/system/config/configuration.template.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Timezone settings */
+$TIMEZONE = '#timezone#';
+date_default_timezone_set($TIMEZONE);
+
+/* Platform URLs settings */
+$SHORT_SITE_NAME = "#short_site_link#";
+$SHORT_SITE_LINK = "#short_site_name#";
+$BASE_URL = "http://#base_url#/";
+$MOBILE_BASE_URL = "http://#mobile_base_url#/";
+
+/* Google Application - Client ID  */
+$GOOGLE_CLIENT_ID = "#google_client_id#";
+
+/* No-Reply email address settings */
+$NOREPLY_MAIL_ADDRESS = '#noreply_email_address#';
+$NOREPLY_MAIL_HOST = '#noreply_email_host#';
+$NOREPLY_MAIL_AUTH = #noreply_email_auth#;
+$NOREPLY_MAIL_USERNAME = '#noreply_email_username#';
+$NOREPLY_MAIL_PASSWORD = '#noreply_email_password#';
+$NOREPLY_MAIL_SECURE_PROTOCOL = '#noreply_email_protocol#';
+$NOREPLY_MAIL_SERVER_PORT = #noreply_email_port#;
+
+/* Platform API settings */
+$WEBAPI_VERSION = '1.0';
+
+/* Caching system settings */
+$CACHE_SYSTEM = 'apc';
+
+?>
