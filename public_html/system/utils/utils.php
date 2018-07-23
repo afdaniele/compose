a6bb8af61b298b62803712596ef95a3246e60fe0
Andrea F. Daniele <afdaniele@ttic.edu>
2018-07-23 14:22:16 -0500

    minor

--- public_html/system/utils/utils.php
+++ public_html/system/utils/utils.php
@@ -197,7 +197,7 @@ function secsToHHMMss( $seconds ){
 }
 
 function human_filesize($bytes, $decimals = 2) {
-    $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+    $size = array('B','KB','MB','GB','TB','PB','EB','ZB','YB');
     $factor = floor((strlen($bytes) - 1) / 3);
     return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
 }
