22045f9f3f8644e4f9780bed3d60738123b4b16b
Andrea F. Daniele <afdaniele@ttic.edu>
2018-07-27 10:50:35 -0500

    bug fixed

--- docker/xenial_apache2_php7/assets/entrypoint.sh
+++ docker/xenial_apache2_php7/assets/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# create configuration.php if it does not exist
+cp -n /var/www/html/public_html/system/config/configuration.default.php /var/www/html/public_html/system/config/configuration.php
+
 # update $BASE_URL and $MOBILE_BASE_URL in system/config
 perl -p -i -e 's/\$BASE_URL.*;/\$BASE_URL = "http:\/\/$ENV{BASEURL}\/";/g' /var/www/html/public_html/system/config/configuration.php
 perl -p -i -e 's/\$MOBILE_BASE_URL.*;/\$MOBILE_BASE_URL = "http:\/\/$ENV{BASEURL}\/";/g' /var/www/html/public_html/system/config/configuration.php
