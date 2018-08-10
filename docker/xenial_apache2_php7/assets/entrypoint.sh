b873fba2c74d7aeae40f7791777ce29a2aa9cbcd
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-09 22:55:10 -0500

    docker: compose installed via entrypoint

--- docker/xenial_apache2_php7/assets/entrypoint.sh
+++ docker/xenial_apache2_php7/assets/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# install or update compose
+git -C /var/www/html pull origin $COMPOSE_VERSION || git clone --depth 1 -b $COMPOSE_VERSION https://github.com/afdaniele/compose.git /var/www/html
+
 # change the ownership of the code
 chown www-data:www-data -R /var/www/html/public_html
 
