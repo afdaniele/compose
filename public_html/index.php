1e00c2fa470016ad89bb6877a806ac887f9156be
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-12 11:35:35 -0500

    add google-signin-client_id meta only if login_enabled

--- public_html/index.php
+++ public_html/index.php
@@ -150,8 +150,14 @@
 
 	<!-- Google API Library -->
 	<script src="https://apis.google.com/js/platform.js"></script>
-	<meta name="google-signin-client_id" content="<?php echo Core::getSetting('google_client_id', 'core') ?>">
-
+	<?php
+	if( Core::getSetting('login_enabled', 'core') ){
+		?>
+		<meta name="google-signin-client_id" content="<?php echo Core::getSetting('google_client_id', 'core') ?>">
+		<?php
+	}
+	?>
+	
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
