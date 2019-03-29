e5e41c6e40fd294d771217d0b4ff8782814a4375
Andrea F. Daniele <afdaniele@ttic.edu>
2019-03-29 01:14:31 -0700

    minor

--- public_html/system/packages/core/modules/footer.php
+++ public_html/system/packages/core/modules/footer.php
@@ -7,51 +7,52 @@
 
 
 use \system\classes\Core;
+use \system\classes\Configuration;
 
 ?>
 
 <style type="text/css">
 
-	#credits_table_center {
-		margin: auto;
-		margin-top: 10px;
-	}
+#credits_table_center {
+  margin: auto;
+  margin-top: 10px;
+}
 
-	#credits_table_right {
-		float: right;
-		margin: 10px 10px 0 0;
-	}
+#credits_table_right {
+  float: right;
+  margin: 10px 10px 0 0;
+}
 
-	#footer_developer_credit > p{
-		font-size: 8pt;
-		width: 190px;
-		text-align: left;
-		margin-bottom: 0;
-		line-height: 11pt;
-	}
+#footer_developer_credit > p{
+  font-size: 8pt;
+  width: 190px;
+  text-align: left;
+  margin-bottom: 0;
+  line-height: 11pt;
+}
 
-	#footer_compose_credit{
-		padding-left: 30px;
-	}
+#footer_compose_credit{
+  padding-left: 30px;
+}
 
-	#footer_compose_credit > p{
-		padding-right: 20px;
-		margin-bottom: 0;
-	}
+#footer_compose_credit > p{
+  padding-right: 20px;
+  margin-bottom: 0;
+}
 
-	#footer_compose_credit > p > span{
-		font-size: 8pt;
-		font-weight: bold;
-	}
+#footer_compose_credit > p > span{
+  font-size: 8pt;
+  font-weight: bold;
+}
 
-	#footer_compose_credit > p > a > img{
-		height: 24px;
-	}
+#footer_compose_credit > p > a > img{
+  height: 24px;
+}
 
-	#footer_compose_separator{
-		font-size: 16pt;
-		padding-right: 20px;
-	}
+#footer_compose_separator{
+  font-size: 16pt;
+  padding-right: 20px;
+}
 
 </style>
 
@@ -71,113 +72,121 @@ function footer_user_menu(){
     $pages_blacklist = ['setup'];
   }
 
-	// get user info
-	$user = Core::getUserLogged();
-	?>
-	<ul class="nav navbar-nav" style="margin-left:10px;">
+  // get user info
+  $user = Core::getUserLogged();
+  ?>
+  <ul class="nav navbar-nav" style="margin-left:10px;">
 
-		<li class="dropup">
+    <li class="dropup">
 
-			<a class="dropdown-toggle cursor-pointer" data-toggle="dropdown" style="padding-top:12px; padding-bottom:0; z-index:999999">
-				<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-				<img src="<?php echo $user['picture']; ?>" id="avatar">
-			</a>
+      <a class="dropdown-toggle cursor-pointer" data-toggle="dropdown" style="padding-top:12px; padding-bottom:0; z-index:999999">
+        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+        <?php
+        $picture_url = $user['picture'];
+        if (preg_match('#^https?://#i', $picture_url) !== 1) {
+          $picture_url = sanitize_url(sprintf(
+            "%s/%s", Configuration::$BASE_URL, $picture_url
+          ));
+        }
+        ?>
+        <img src="<?php echo $picture_url; ?>" id="avatar">
+      </a>
 
-			<ul class="dropdown-menu dropup" role="menu" style="background-color:#3c3c3c; color:white">
-				<?php
-				// get pages
-				$main_user_role = Core::getUserRole();
-				$user_roles = Core::getUserRolesList();
-				$pages = Core::getFilteredPagesList(
-					'by-menuorder',
-					true /* enabledOnly */,
-					$user_roles /* accessibleBy */
-				);
+      <ul class="dropdown-menu dropup" role="menu" style="background-color:#3c3c3c; color:white">
+        <?php
+        // get pages
+        $main_user_role = Core::getUserRole();
+        $user_roles = Core::getUserRolesList();
+        $pages = Core::getFilteredPagesList(
+          'by-menuorder',
+          true /* enabledOnly */,
+          $user_roles /* accessibleBy */
+        );
 
-				foreach($pages as &$elem) {
+        foreach($pages as &$elem) {
           if( !is_null($pages_whitelist) && !in_array($elem['id'], $pages_whitelist) )
-            continue;
+          continue;
           if( !is_null($pages_blacklist) && in_array($elem['id'], $pages_blacklist) )
-            continue;
-					// hide pages if maintenance mode is enabled
-					if( $main_user_role!='administrator' && Core::getSetting('maintenance_mode','core',true) && $elem['id']!='login' )
-						continue;
-					// hide page if the current user' role is excluded
-					if( count( array_intersect($user_roles, $elem['menu_entry']['exclude_roles']) ) > 0 )
-						continue;
-					$icon = sprintf('%s %s-%s', $elem['menu_entry']['icon']['class'], $elem['menu_entry']['icon']['class'], $elem['menu_entry']['icon']['name']);
-					?>
-					<li role="presentation">
-						<a role="menuitem" tabindex="-1"
-						   href="<?php echo \system\classes\Configuration::$BASE . $elem['id'] ?>"
-						   style="color:white" onMouseOver="this.style.color='#000'" onMouseOut="this.style.color='#FFF'">
-								<span class="<?php echo $icon ?>" aria-hidden="true"></span> &nbsp;
-								<?php echo $elem['name'] ?>
-						</a>
-					</li>
-					<?php
-				}
-				?>
-				<li class="divider"></li>
-				<!-- Logout button -->
-				<li role="presentation" style="margin-bottom:20px">
-					<a role="menuitem" tabindex="-1" href="#"
-					   onclick="userLogOut('<?php echo \system\classes\Configuration::$BASE ?>', '<?php echo \system\classes\Configuration::$BASE_URL ?>', '<?php echo \system\classes\Configuration::$WEBAPI_VERSION ?>', '<?php echo $_SESSION['TOKEN'] ?>');"
-					   style="color:#ffc864" onMouseOver="this.style.color='#000'" onMouseOut="this.style.color='#ffc864'">
-							<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> &nbsp;Log out
-					</a>
-				</li>
-			</ul>
+          continue;
+          // hide pages if maintenance mode is enabled
+          if( $main_user_role!='administrator' && Core::getSetting('maintenance_mode','core',true) && $elem['id']!='login' )
+          continue;
+          // hide page if the current user' role is excluded
+          if( count( array_intersect($user_roles, $elem['menu_entry']['exclude_roles']) ) > 0 )
+          continue;
+          $icon = sprintf('%s %s-%s', $elem['menu_entry']['icon']['class'], $elem['menu_entry']['icon']['class'], $elem['menu_entry']['icon']['name']);
+          ?>
+          <li role="presentation">
+            <a role="menuitem" tabindex="-1"
+            href="<?php echo \system\classes\Configuration::$BASE . $elem['id'] ?>"
+            style="color:white" onMouseOver="this.style.color='#000'" onMouseOut="this.style.color='#FFF'">
+            <span class="<?php echo $icon ?>" aria-hidden="true"></span> &nbsp;
+            <?php echo $elem['name'] ?>
+          </a>
+        </li>
+        <?php
+      }
+      ?>
+      <li class="divider"></li>
+      <!-- Logout button -->
+      <li role="presentation" style="margin-bottom:20px">
+        <a role="menuitem" tabindex="-1" href="#"
+        onclick="userLogOut('<?php echo \system\classes\Configuration::$BASE ?>', '<?php echo \system\classes\Configuration::$BASE_URL ?>', '<?php echo \system\classes\Configuration::$WEBAPI_VERSION ?>', '<?php echo $_SESSION['TOKEN'] ?>');"
+        style="color:#ffc864" onMouseOver="this.style.color='#000'" onMouseOut="this.style.color='#ffc864'">
+        <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> &nbsp;Log out
+      </a>
+    </li>
+  </ul>
 
-		</li>
+</li>
 
-	</ul>
+</ul>
 
-	<div style="float:left">
-		<p style="margin:0; font-size:16px; margin-top:8px"><strong><?php echo $user['name'] ?></strong></p>
-		<p style="margin:0; font-size:12px; margin-top:-4px"><?php echo $user['email'] ?></p>
-	</div>
-	<?php
+<div style="float:left">
+  <p style="margin:0; font-size:16px; margin-top:8px"><strong><?php echo $user['name'] ?></strong></p>
+  <p style="margin:0; font-size:12px; margin-top:-4px"><?php echo $user['email'] ?></p>
+</div>
+<?php
 }
 
 
 function footer_credits( $float ){
-	?>
-	<table id="credits_table_<?php echo $float ?>">
-		<tr>
-			<td id="footer_compose_credit">
-				<p>
-					<span>
-						powered by &nbsp;
-					</span>
-					<a href="https://github.com/afdaniele/compose" target="_blank">
-						<img src="<?php echo \system\classes\Configuration::$BASE ?>images/compose-white-logo.svg"></img>
-					</a>
-				</p>
-			</td>
+  ?>
+  <table id="credits_table_<?php echo $float ?>">
+    <tr>
+      <td id="footer_compose_credit">
+        <p>
+          <span>
+            powered by &nbsp;
+          </span>
+          <a href="https://github.com/afdaniele/compose" target="_blank">
+            <img src="<?php echo \system\classes\Configuration::$BASE ?>images/compose-white-logo.svg"></img>
+          </a>
+        </p>
+      </td>
 
-			<td id="footer_compose_separator">
-				&nbsp;|&nbsp;
-			</td>
+      <td id="footer_compose_separator">
+        &nbsp;|&nbsp;
+      </td>
 
-			<td id="footer_developer_credit">
-				<?php
-				$codebase_info = Core::getCodebaseInfo();
-				$codebase_hash = $codebase_info['head_hash'];
-				$codebase_tag = $codebase_info['head_tag'];
-				$codebase_latest_tag = $codebase_info['latest_tag'];
-				$codebase_tag = ( strcasecmp($codebase_tag, $codebase_latest_tag) === 0 )? $codebase_tag : 'devel';
-				$codebase_str = ( in_array($codebase_tag, ['ND', 'GIT_ERROR', null]) )? '' : sprintf("%s | ", $codebase_tag);
-				?>
-				<p>
-					<strong>developed by</strong> &nbsp; <a href="http://www.afdaniele.com" style="color:white">Andrea F. Daniele</a>
-					<br/>
-					<strong>serial</strong> &nbsp; <span style="font-family:monospace">git | <?php echo $codebase_str.$codebase_hash ?></span>
-				</p>
-			</td>
-		</tr>
-	</table>
-	<?php
+      <td id="footer_developer_credit">
+        <?php
+        $codebase_info = Core::getCodebaseInfo();
+        $codebase_hash = $codebase_info['head_hash'];
+        $codebase_tag = $codebase_info['head_tag'];
+        $codebase_latest_tag = $codebase_info['latest_tag'];
+        $codebase_tag = ( strcasecmp($codebase_tag, $codebase_latest_tag) === 0 )? $codebase_tag : 'devel';
+        $codebase_str = ( in_array($codebase_tag, ['ND', 'GIT_ERROR', null]) )? '' : sprintf("%s | ", $codebase_tag);
+        ?>
+        <p>
+          <strong>developed by</strong> &nbsp; <a href="http://www.afdaniele.com" style="color:white">Andrea F. Daniele</a>
+          <br/>
+          <strong>serial</strong> &nbsp; <span style="font-family:monospace">git | <?php echo $codebase_str.$codebase_hash ?></span>
+        </p>
+      </td>
+    </tr>
+  </table>
+  <?php
 }
 
 ?>
@@ -189,31 +198,31 @@ function footer_credits( $float ){
 
 <footer id="footer" class="footer navbar-inverse" style="height:50px; color:#c3c3c3">
 
-	<table style="width:100%">
-		<tr>
+  <table style="width:100%">
+    <tr>
 
-		<?php
-		if( Core::isUserLoggedIn() ){
-		?>
-			<td class="text-left">
-				<?php footer_user_menu(); ?>
-			</td>
+      <?php
+      if( Core::isUserLoggedIn() ){
+        ?>
+        <td class="text-left">
+          <?php footer_user_menu(); ?>
+        </td>
 
-			<td class="text-right">
-				<?php footer_credits('right'); ?>
-			</td>
-		<?php
-		}else{
-		?>
-			<td>
-				<?php footer_credits('center'); ?>
-			</td>
-		<?php
-		}
-		?>
+        <td class="text-right">
+          <?php footer_credits('right'); ?>
+        </td>
+        <?php
+      }else{
+        ?>
+        <td>
+          <?php footer_credits('center'); ?>
+        </td>
+        <?php
+      }
+      ?>
 
-		</tr>
-	</table>
+    </tr>
+  </table>
 
 
 </footer>
--- public_html/system/packages/core/pages/profile/index.php
+++ public_html/system/packages/core/pages/profile/index.php
@@ -7,67 +7,76 @@
 
 
 use \system\classes\Core;
+use \system\classes\Configuration;
 
 ?>
 
 <div style="width:100%; margin:auto">
 
-	<table style="width:100%; border-bottom:1px solid #ddd; margin-bottom:32px">
+  <table style="width:100%; border-bottom:1px solid #ddd; margin-bottom:32px">
 
-		<tr>
-			<td style="width:50%">
-				<h2>Your account</h2>
-			</td>
-		</tr>
+    <tr>
+      <td style="width:50%">
+        <h2>Your account</h2>
+      </td>
+    </tr>
 
-	</table>
+  </table>
 
-	<?php
+  <?php
 
-	// get core info
-	$user = Core::getUserLogged();
+  // get core info
+  $user = Core::getUserLogged();
 
-	// prepare labels and values
-	$labelName = array('Name', 'E-mail address', 'Account type' );
-	$fieldValue = array( $user['name'], $user['email'], ucfirst($user['role']) );
+  // prepare labels and values
+  $labelName = array('Name', 'E-mail address', 'Account type' );
+  $fieldValue = array( $user['name'], $user['email'], ucfirst($user['role']) );
 
-	// get roles in packages
-	$packages = array_keys( Core::getPackagesList() );
-	foreach($packages as $package) {
-		if( $package == 'core' ) continue;
-		$role = Core::getUserRole( $package );
-		if( !is_null($role) ){
-			array_push($labelName, sprintf('Role (%s)',$package));
-			array_push($fieldValue, $role);
-		}
-	}
+  // get roles in packages
+  $packages = array_keys( Core::getPackagesList() );
+  foreach($packages as $package) {
+    if( $package == 'core' ) continue;
+    $role = Core::getUserRole( $package );
+    if( !is_null($role) ){
+      array_push($labelName, sprintf('Role (%s)',$package));
+      array_push($fieldValue, $role);
+    }
+  }
 
-	?>
+  ?>
 
-	<h4>Personal Information</h4>
-	<nav class="navbar navbar-default" role="navigation" style="margin-bottom:36px">
-		<div class="container-fluid" style="padding-left:0; padding-right:0">
+  <h4>Personal Information</h4>
+  <nav class="navbar navbar-default" role="navigation" style="margin-bottom:36px">
+    <div class="container-fluid" style="padding-left:0; padding-right:0">
 
-			<div class="collapse navbar-collapse navbar-left" style="padding:0; width:100%">
+      <div class="collapse navbar-collapse navbar-left" style="padding:0; width:100%">
 
-				<table style="width:100%">
-					<tr>
-						<td class="col-md-3 text-center" style="border-right:1px solid lightgray">
-							<h3 style="margin:0">
-								<div class="text-center col-md-12" id="profile_page_avatar">
-									<img src="<?php echo $user['picture']; ?>" id="avatar">
-								</div>
-							</h3>
-						</td>
-						<td class="col-md-9" style="padding:20px">
-							<?php
-							generateView( $labelName, $fieldValue, 'md-3', 'md-9' );
-							?>
-						</td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</nav>
+        <table style="width:100%">
+          <tr>
+            <td class="col-md-3 text-center" style="border-right:1px solid lightgray">
+              <h3 style="margin:0">
+                <div class="text-center col-md-12" id="profile_page_avatar">
+                  <?php
+                  $picture_url = $user['picture'];
+                  if (preg_match('#^https?://#i', $picture_url) !== 1) {
+                    $picture_url = sanitize_url(sprintf(
+                      "%s/%s", Configuration::$BASE_URL, $picture_url
+                    ));
+                  }
+                  ?>
+                  <img src="<?php echo $picture_url; ?>" id="avatar">
+                </div>
+              </h3>
+            </td>
+            <td class="col-md-9" style="padding:20px">
+              <?php
+              generateView( $labelName, $fieldValue, 'md-3', 'md-9' );
+              ?>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </nav>
 
 </div>
