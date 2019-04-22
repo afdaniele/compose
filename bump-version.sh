b93cf6b712f62d170c1c2bf8381b1d81c0ed0437
Andrea F. Daniele <afdaniele@ttic.edu>
2019-04-21 19:56:50 -0700

    minor

--- bump-version.sh
+++ bump-version.sh
@@ -68,6 +68,15 @@ function tag {
 }
 
 if [ -f VERSION ]; then
+    # Check if your current source is not already tagged by using current hash
+    GIT_COMMIT=`git rev-parse HEAD`
+    NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+    # Only tag if no tag already (would be better if the git describe command above could have a silent option)
+    if [ -n "$NEEDS_TAG" ]; then
+        echo -e "${WARNING_FLAG} Current code is already released."
+        exit 0
+    fi
+    # read current version from VERSION file
     BASE_STRING=`cat VERSION`
     BASE_LIST=(`echo $BASE_STRING | tr '.' ' '`)
     V_MAJOR=${BASE_LIST[0]}
@@ -86,14 +95,6 @@ if [ -f VERSION ]; then
     read INPUT_STRING
     if [ "$INPUT_STRING" = "" ]; then
         INPUT_STRING=$SUGGESTED_VERSION
-    fi
-    # Check if your current source is not already tagged by using current hash
-    GIT_COMMIT=`git rev-parse HEAD`
-    NEEDS_TAG=`git describe --contains $GIT_COMMIT`
-    # Only tag if no tag already (would be better if the git describe command above could have a silent option)
-    if [ -n "$NEEDS_TAG" ]; then
-        echo -e "${WARNING_FLAG} Current code is already released."
-        exit 0
     fi
     echo -e "${NOTICE_FLAG} Will set new version to be ${WHITE}$INPUT_STRING"
     echo $INPUT_STRING > VERSION
@@ -135,4 +136,4 @@ else
     fi
 fi
 
-echo -e "${NOTICE_FLAG} Finished."
+echo -e "${NOTICE_FLAG} Finished.${RESET}"
