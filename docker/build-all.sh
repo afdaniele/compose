63770e9b119b11a30c2a4d84bb965d1a2d7e5e34
Andrea F. Daniele <afdaniele@ttic.edu>
2019-07-23 22:20:02 +0200

    refactored docker files

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker build -t afdaniele/compose:latest ./master
-docker build -t afdaniele/compose:devel ./devel
+docker build -t afdaniele/compose:devel -f ./master/Dockerfile.devel ./master
+
 docker build -t afdaniele/compose-arm32v7:latest ./arm32v7
+docker build -t afdaniele/compose-arm32v7:devel -f ./arm32v7/Dockerfile.devel ./arm32v7
