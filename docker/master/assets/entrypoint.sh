1c57eb5e71cb2171a83134223efddf1eaa8d42d2
Andrea F. Daniele <afdaniele@ttic.edu>
2019-09-14 14:04:55 -0500

    simplified SSL configuration

--- docker/master/assets/entrypoint.sh
+++ docker/master/assets/entrypoint.sh
@@ -21,38 +21,19 @@ echo "Done!"
 # check if SSL is enabled and the keys are provided
 if [ "${SSL}" == "1" ]; then
   echo "Enabling SSL..."
-  # create directory for apache to get the ssl files
-  mkdir -p "${ssldir_apache}"
   # enable SSL website
   a2ensite 000-default-ssl
-  # check if SSL_CERTFILE was passed
-  if [ -z ${SSL_CERTFILE+x} ]; then
-    echo "The environment variable [SSL_CERTFILE] is required"
-    echo "Exiting..."
-    exit 1
-  fi
-  # check if SSL_KEYFILE was passed
-  if [ -z ${SSL_KEYFILE+x} ]; then
-    echo "The environment variable [SSL_KEYFILE] is required"
-    echo "Exiting..."
-    exit 2
-  fi
-  # check values
+  # check files
   if [[ ! -e "${SSL_CERTFILE}" ]]; then
     echo "The certificate file [${SSL_CERTFILE}] is invalid or it is a broken symlink."
     echo "Exiting..."
-    exit 3
+    exit 1
   fi
   if [[ ! -e "${SSL_KEYFILE}" ]]; then
     echo "The certificate key [${SSL_KEYFILE}] is invalid or it is a broken symlink."
     echo "Exiting..."
-    exit 4
+    exit 2
   fi
-  # create symlink for apache
-  _SSL_CERTFILE=`readlink -f "${SSL_CERTFILE}"`
-  _SSL_KEYFILE=`readlink -f "${SSL_KEYFILE}"`
-  ln -s ${_SSL_CERTFILE} ${ssldir_apache}/fullchain.pem
-  ln -s ${_SSL_KEYFILE} ${ssldir_apache}/privkey.pem
   echo "Done!"
 fi
 
