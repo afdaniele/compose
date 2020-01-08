319908b850934bc9c35ac95bb2608000b5280778
Andrea F. Daniele <afdaniele@ttic.edu>
2020-01-08 14:56:41 -0600

    changed (image|css|js|script).php to use user-data

--- public_html/scripts/image.php
+++ public_html/scripts/image.php
@@ -4,6 +4,9 @@
 # @Last modified by:   afdaniele
 
 
+require_once __DIR__.'/../system/environment.php';
+
+
 // get info
 $packageName = $_GET['package'];
 $imageName = $_GET['image'];
@@ -13,28 +16,36 @@ $packageName = preg_replace('/[^a-z0-9_-]/', '', $packageName);
 $imageName = preg_replace('/[^a-zA-Z0-9_.-]/', '', $imageName);
 
 // check whether the image exists
-$imagePath = sprintf("%s/../system/packages/%s/images/%s", __DIR__, $packageName, $imageName);
-if( !file_exists($imagePath) ){
-    $imagePath = sprintf("%s/../images/placeholder.png", __DIR__);
+$imagePaths = [
+  sprintf("%s/../system/packages/%s/images/%s", __DIR__, $packageName, $imageName),
+  sprintf("%s%s/images/%s", $GLOBALS['__USERDATA__PACKAGES__DIR__'], $packageName, $imageName),
+  sprintf("%s/../images/placeholder.png", __DIR__)
+];
+
+foreach ($imagePaths as $imagePath) {
+  // if this does not exist, try the next
+  if (!file_exists($imagePath)) {
+    continue;
+  }
+
+  // open the file in a binary mode
+  $fp = fopen($imagePath, 'rb');
+  $fsize = filesize($imagePath);
+
+  // get info about the image
+  $imageInfo = getimagesize($imagePath);
+  if( strpos($imageName, '.svg') !== false ){
+      $imageInfo['mime'] = 'image/svg+xml';
+  }
+
+  // clean buffer
+  if (ob_get_length()) ob_clean();
+
+  // send the right headers
+  header( sprintf("Content-Type: %s", $imageInfo['mime']), true );
+  header( sprintf("Content-Length: %s", $fsize), true );
+
+  // dump the picture and stop the script
+  fpassthru($fp);
+  exit;
 }
-
-// open the file in a binary mode
-$fp = fopen($imagePath, 'rb');
-$fsize = filesize($imagePath);
-
-// get info about the image
-$imageInfo = getimagesize($imagePath);
-if( strpos($imageName, '.svg') !== false ){
-    $imageInfo['mime'] = 'image/svg+xml';
-}
-
-// clean buffer
-if (ob_get_length()) ob_clean();
-
-// send the right headers
-header( sprintf("Content-Type: %s", $imageInfo['mime']), true );
-header( sprintf("Content-Length: %s", $fsize), true );
-
-// dump the picture and stop the script
-fpassthru($fp);
-exit;
