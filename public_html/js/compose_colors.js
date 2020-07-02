cf0850acc8f2b16ff933cf8a451cc911db2db7c3
Andrea F. Daniele <afdaniele@ttic.edu>
2020-07-02 17:50:31 -0500

    added new compose color palette

---
+++ public_html/js/compose_colors.js
@@ -0,0 +1,198 @@
+window.compose.colors = {
+    "red light-5": "#ffebee",
+    "red-light-4": "#ffcdd2",
+    "red-light-3": "#ef9a9a",
+    "red-light-2": "#e57373",
+    "red-light-1": "#ef5350",
+    "red-standard-1": "#f44336",
+    "red-dark-1": "#e53935",
+    "red-dark-2": "#d32f2f",
+    "red-dark-3": "#c62828",
+    "red-dark-4": "#b71c1c",
+
+    "pink-light-5": "#fce4ec",
+    "pink-light-4": "#f8bbd0",
+    "pink-light-3": "#f48fb1",
+    "pink-light-2": "#f06292",
+    "pink-light-1": "#ec407a",
+    "pink-standard-1": "#e91e63",
+    "pink-dark-1": "#d81b60",
+    "pink-dark-2": "#c2185b",
+    "pink-dark-3": "#ad1457",
+    "pink-dark-4": "#880e4f",
+
+    "purple-light-5": "#f3e5f5",
+    "purple-light-4": "#e1bee7",
+    "purple-light-3": "#ce93d8",
+    "purple-light-2": "#ba68c8",
+    "purple-light-1": "#ab47bc",
+    "purple-standard-1": "#9c27b0",
+    "purple-dark-1": "#8e24aa",
+    "purple-dark-2": "#7b1fa2",
+    "purple-dark-3": "#6a1b9a",
+    "purple-dark-4": "#4a148c",
+
+    "indigo-light-5": "#e8eaf6",
+    "indigo-light-4": "#c5cae9",
+    "indigo-light-3": "#9fa8da",
+    "indigo-light-2": "#7986cb",
+    "indigo-light-1": "#5c6bc0",
+    "indigo-standard-1": "#3f51b5",
+    "indigo-dark-1": "#3949ab",
+    "indigo-dark-2": "#303f9f",
+    "indigo-dark-3": "#283593",
+    "indigo-dark-4": "#1a237e",
+
+    "blue-light-5": "#e3f2fd",
+    "blue-light-4": "#bbdefb",
+    "blue-light-3": "#90caf9",
+    "blue-light-2": "#64b5f6",
+    "blue-light-1": "#42a5f5",
+    "blue-standard-1": "#2196f3",
+    "blue-dark-1": "#1e88e5",
+    "blue-dark-2": "#1976d2",
+    "blue-dark-3": "#1565c0",
+    "blue-dark-4": "#0d47a1",
+
+    "cyan-light-5": "#e0f7fa",
+    "cyan-light-4": "#b2ebf2",
+    "cyan-light-3": "#80deea",
+    "cyan-light-2": "#4dd0e1",
+    "cyan-light-1": "#26c6da",
+    "cyan-standard-1": "#00bcd4",
+    "cyan-dark-1": "#00acc1",
+    "cyan-dark-2": "#0097a7",
+    "cyan-dark-3": "#00838f",
+    "cyan-dark-4": "#006064",
+
+    "teal-light-5": "#e0f2f1",
+    "teal-light-4": "#b2dfdb",
+    "teal-light-3": "#80cbc4",
+    "teal-light-2": "#4db6ac",
+    "teal-light-1": "#26a69a",
+    "teal-standard-1": "#009688",
+    "teal-dark-1": "#00897b",
+    "teal-dark-2": "#00796b",
+    "teal-dark-3": "#00695c",
+    "teal-dark-4": "#004d40",
+
+    "green-light-5": "#e8f5e9",
+    "green-light-4": "#c8e6c9",
+    "green-light-3": "#a5d6a7",
+    "green-light-2": "#81c784",
+    "green-light-1": "#66bb6a",
+    "green-standard-1": "#4caf50",
+    "green-dark-1": "#43a047",
+    "green-dark-2": "#388e3c",
+    "green-dark-3": "#2e7d32",
+    "green-dark-4": "#1b5e20",
+
+    "lime-light-5": "#f9fbe7",
+    "lime-light-4": "#f0f4c3",
+    "lime-light-3": "#e6ee9c",
+    "lime-light-2": "#dce775",
+    "lime-light-1": "#d4e157",
+    "lime-standard-1": "#cddc39",
+    "lime-dark-1": "#c0ca33",
+    "lime-dark-2": "#afb42b",
+    "lime-dark-3": "#9e9d24",
+    "lime-dark-4": "#827717",
+
+    "yellow-light-5": "#fffde7",
+    "yellow-light-4": "#fff9c4",
+    "yellow-light-3": "#fff59d",
+    "yellow-light-2": "#fff176",
+    "yellow-light-1": "#ffee58",
+    "yellow-standard-1": "#ffeb3b",
+    "yellow-dark-1": "#fdd835",
+    "yellow-dark-2": "#fbc02d",
+    "yellow-dark-3": "#f9a825",
+    "yellow-dark-4": "#f57f17",
+
+    "orange-light-5": "#fff3e0",
+    "orange-light-4": "#ffe0b2",
+    "orange-light-3": "#ffcc80",
+    "orange-light-2": "#ffb74d",
+    "orange-light-1": "#ffa726",
+    "orange-standard-1": "#ff9800",
+    "orange-dark-1": "#fb8c00",
+    "orange-dark-2": "#f57c00",
+    "orange-dark-3": "#ef6c00",
+    "orange-dark-4": "#e65100",
+
+    "brown-light-5": "#efebe9",
+    "brown-light-4": "#d7ccc8",
+    "brown-light-3": "#bcaaa4",
+    "brown-light-2": "#a1887f",
+    "brown-light-1": "#8d6e63",
+    "brown-standard-1": "#795548",
+    "brown-dark-1": "#6d4c41",
+    "brown-dark-2": "#5d4037",
+    "brown-dark-3": "#4e342e",
+    "brown-dark-4": "#3e2723",
+
+    "grey-light-5": "#fafafa",
+    "grey-light-4": "#f5f5f5",
+    "grey-light-3": "#eeeeee",
+    "grey-light-2": "#e0e0e0",
+    "grey-light-1": "#bdbdbd",
+    "grey-standard-1": "#9e9e9e",
+    "grey-dark-1": "#757575",
+    "grey-dark-2": "#616161",
+    "grey-dark-3": "#424242",
+    "grey-dark-4": "#212121",
+
+    "gray-light-5": "#fafafa",
+    "gray-light-4": "#f5f5f5",
+    "gray-light-3": "#eeeeee",
+    "gray-light-2": "#e0e0e0",
+    "gray-light-1": "#bdbdbd",
+    "gray-standard-1": "#9e9e9e",
+    "gray-dark-1": "#757575",
+    "gray-dark-2": "#616161",
+    "gray-dark-3": "#424242",
+    "gray-dark-4": "#212121"
+};
+
+
+window.compose.palette = [
+    "blue",
+    "green",
+    "red",
+    "yellow",
+    "purple",
+    "cyan",
+    "teal",
+    "orange",
+    "brown",
+    "pink",
+    "lime",
+    "indigo"
+];
+
+class ColorPalette{
+    constructor() {
+        this.i = 0;
+    }
+
+    current(tone = 'standard', level = 1){
+        let color = window.compose.palette[this.i];
+        return ColorPalette.get(color, tone, level);
+    }
+
+    next(tone = 'standard', level = 1){
+        let color = this.current(tone, level);
+        this.i = (this.i + 1) % window.compose.palette.length;
+        return color;
+    }
+
+    static get(color, tone = 'standard', level = 1) {
+        let color_key = "{0}-{1}-{2}".format(color, tone, level);
+        return window.compose.colors[color_key];
+    }
+
+    static geti(i, tone = 'standard', level = 1) {
+        let idx = i % window.compose.palette.length;
+        return ColorPalette.get(window.compose.palette[idx], tone, level);
+    }
+}
