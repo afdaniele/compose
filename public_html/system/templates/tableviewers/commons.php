f13e9e07133b9feca768b03a1c5f75ee733390fc
Andrea F. Daniele <afdaniele@ttic.edu>
2022-01-02 01:52:58 -0600

    bug fix w/ PHP 8.1

--- public_html/system/templates/tableviewers/commons.php
+++ public_html/system/templates/tableviewers/commons.php
@@ -7,7 +7,7 @@ function generateDescriptionListView($labelName, $fieldValue, $labelColumn, $val
             $label_col = (is_array($labelColumn)) ? $labelColumn[$i] : $labelColumn;
             $value_col = (is_array($valueColumn)) ? $valueColumn[$i] : $valueColumn;
             $label = $labelName[$i];
-            $value = (strlen($fieldValue[$i]) > 0) ? $fieldValue[$i] : '&nbsp;';
+            $value = (!is_null($fieldValue[$i]) && strlen($fieldValue[$i]) > 0) ? $fieldValue[$i] : '&nbsp;';
             ?>
             <dt class="col-sm-<?php echo $label_col ?>"><?php echo $label ?></dt>
             <dd class="col-sm-<?php echo $value_col ?>"><?php echo $value ?></dd>
